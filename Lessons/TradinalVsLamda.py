6fab5d5d29b54407f6a304cd15a438593819997e
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-21 08:23:35 +0100

    Tadional vs Lambda Function

--- Lessons/TradinalVsLamda.py
+++ Lessons/TradinalVsLamda.py
@@ -4,7 +4,13 @@ def count(x):
     y = x + 10
     return y
 
+print("With Traditional Function")
 print(count(10))
+print()
 
 
 # Lamda Function
+x=lambda a:a+10
+
+print("With Lamda Function")
+print(x(5))
