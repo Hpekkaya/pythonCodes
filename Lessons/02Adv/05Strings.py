a9c2dbef65324229b22a12bb147da9359a381823
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-22 08:38:07 +0100

    String Iterate process applied

--- Lessons/02Adv/05Strings.py
+++ Lessons/02Adv/05Strings.py
@@ -17,5 +17,50 @@
 # sub_string4 = my_string[::-1] #Reverse
 # print("4 :", sub_string4)
 
-my_string = "Hello "
+#String Iteration
+greeting = "Hello "
 name = "Hakan"
+
+sentence = greeting + " " + name
+print(sentence)
+
+# for i in greeting:
+#     print(i)
+    
+# if "e" in sentence:
+#     print("Yes")
+# else : print("No")
+
+# my_string = "    Hello   "
+# my_string= my_string.strip() #remove the space at the beginning and end
+
+# print(my_string)
+# print(my_string.upper())
+# print(my_string.lower())
+# print(my_string)
+# my_string = my_string.upper()
+# print(my_string)
+
+# my_string = "Hello World"
+# print(my_string.startswith("H")) 
+# print(my_string.endswith("d")) 
+
+# print(my_string.find("llo")) 
+# print(my_string.find("k")) # If there is not returns -1
+
+# print(my_string.count("l")) 
+
+# print(my_string.replace("World","Dears"))
+
+my_string = "How are you doing ?"
+my_list = my_string.split()  #Split into words
+print(my_string, my_list)
+
+my_string1 = "How, are, you, doing ?"
+my_list1 = my_string1.split()  #Split into words and ","
+my_list11 = my_string1.split(",")  #Split into words and ","
+print(my_string1, my_list11)
+
+
+
+
