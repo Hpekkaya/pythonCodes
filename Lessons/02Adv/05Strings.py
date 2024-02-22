07bce522e8d2b328b5571b3784fc6ba83ae55ef7
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-22 19:48:45 +0100

    String Formatting methods applied

--- Lessons/02Adv/05Strings.py
+++ Lessons/02Adv/05Strings.py
@@ -68,14 +68,14 @@
 # print(new_string1)
 
 
-my_list =["a"]*1000
+# my_list =["a"]*10
 # print(my_list)
 
-from timeit import default_timer as timer
+# from timeit import default_timer as timer
 
-start = timer()
-#Bad Code
-my_string =''
+# start = timer()
+# #Bad Code
+# my_string =''
 # for i in my_list:
 #     my_string += i
 
@@ -91,4 +91,21 @@ my_string =''
 # time = stop-start
 # print(time*1000)
 
+# String Formatting
+var = 3
+var1 = 3.1258
+my_string ="The variable is %.0f" % var
+my_string1 ="The variable is %.4f" % var
+my_string2 ="The variable is {}, {:.2f} and {}".format(var,var,var1)
+my_string3 =f"The variable is {var}, {var1} and {var*2}"
+
+print(my_string)
+print(my_string1)
+print(my_string2)
+print(my_string3)
+
+var = "Tom"
+my_string ="The variable is %s" % var
+
+print(my_string)
 
