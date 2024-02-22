e2005f0f3d8e2cdab06f54838ac4ba7c54f9a3fc
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-22 07:47:34 +0100

    Basic strin process (assignmet, etc) applied

---
+++ Lessons/02Adv/05Strings.py
@@ -0,0 +1,21 @@
+#Strings ordered, immutable, text representation
+
+# my_string = "Hello World"
+# my_string[8] = "T"  #TypeError: 'str' object does not support item assignment
+# my_string = "Hello Turkey"  #Support assignment 
+# print(my_string)
+
+# sub_string = my_string[3:9]
+# print(sub_string)
+
+# sub_string2 = my_string[6::2] #from 6 Step 2
+# print(sub_string2)
+
+# sub_string3 = my_string[4::] #from 4 Step 1 (Default)
+# print("3 :", sub_string3)
+
+# sub_string4 = my_string[::-1] #Reverse
+# print("4 :", sub_string4)
+
+my_string = "Hello "
+name = "Hakan"
