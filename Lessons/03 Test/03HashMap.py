327f45e0ea500c2f56bc523c6e2f07f596fe8d58
Hpekkaya <Ahpek1970@yahoo.com>
2024-02-23 13:14:11 +0100

    Class Methods (from string) applied

---
+++ Lessons/03 Test/03HashMap.py
@@ -0,0 +1,17 @@
+# nums = (2, 7, 11, 15)
+# target = 9
+
+
+# class Solution:
+# def twoSum(self, nums: list[int], target: int) -> List[int]:
+#     prevMap = {}  # val:index
+
+#     for i, n in enumarate(nums):
+#         diff = target - n
+#         if diff in prevMap:
+#             return [prevMap[diff], i]
+#         prevMap[n] = i
+#     return
+
+
+# print(twoSum(nums, target))
--- Lessons/04 OOP/03 ClassMethods_StaticMethods.py
+++ Lessons/04 OOP/03 ClassMethods_StaticMethods.py
@@ -5,6 +5,7 @@
 
 class Employee:
 
+    num_of_emps = 1
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
@@ -13,26 +14,34 @@ class Employee:
         self.email = first + "." + last + "@mail.com"
         self.pay = pay
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
+
 
 emp_1 = Employee("Hakan", "Pekkaya", 20000)
 emp_2 = Employee("Test", "Hakan", 25000)
 
-# print("Before raise :", emp_1.pay)
-# emp_1.apply_raise()
-# print("After raise  :", emp_1.pay)
 
-# print(Employee.__dict__)
+emp_str_1 = "John-Doe-40000"
+emp_str_2 = "Mary-Wane-30000"
+emp_str_3 = "Elon-Tart-20000"
 
-print(Employee.num_of_emps)
-Employee.raise_amount = 1.05
 
-print(emp_1.__dict__)
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
