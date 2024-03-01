09e4533d4ac9cd52a0125e72010e29cf31b18e16
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-01 10:29:21 +0100

    Collections(orderdDict, defaultDict, deque) applied

--- Lessons/02Adv/06Collections.py
+++ Lessons/02Adv/06Collections.py
@@ -19,12 +19,70 @@ from collections import namedtuple
 
 # # namedtuple ***************
 
-Point = namedtuple("Point", "x,y")
-pt = Point(-1, 4)
-print(pt)
-print(pt.x, pt.y)
+# Point = namedtuple("Point", "x,y")
+# pt = Point(-1, 4)
+# print(pt)
+# print(pt.x, pt.y)
 
 
 from collections import OrderedDict
 
 # # OrderdDict ***************
+# ordered_dict = OrderedDict()
+# ordered_dict["a"] = 1
+# ordered_dict["b"] = 2
+
+# ordered_dict["c"] = 3
+# ordered_dict["d"] = 4
+# ordered_dict["a"] = 5
+
+# print(ordered_dict)
+
+from collections import defaultdict
+
+# d = defaultdict(float)  #Can be int, list
+# d["a"] = 1
+# d["b"] = 2
+# print(d)
+# print(d["a"], d["b"])
+# print(d["c"])  # If not assigned, Default value 0, if float 0.0
+
+# d = defaultdict(list)
+# d["a"] = 1
+# d["b"] = 2
+# print(d)
+# print(d["a"], d["b"])
+# print(d["c"])  # If not assigned, Default value []
+
+# d = defaultdict(list)
+
+# for i in range(5):
+#     d[i].append(i)
+
+# print("Dictionary with values as list:")
+# print(d)
+
+
+from collections import deque
+
+d = deque()
+
+d.append(1)
+d.append(2)
+d.appendleft(3)
+print(d)
+
+d.pop()
+print(d)
+
+d.popleft()
+print(d)
+
+d.extendleft([4, 5, 6, 8])
+print(d)
+
+d.reverse()
+print(d)
+
+d.rotate(6)
+print(d)
