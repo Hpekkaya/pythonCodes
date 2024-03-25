6a00708c681f9f9049a7138b7e1cac000b7562b1
Hpekkaya <Ahpek1970@yahoo.com>
2024-03-25 16:48:34 +0300

    New

---
+++ Lessons/08DataStructure/O(n).py
@@ -0,0 +1,18 @@
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                print(i+1,j+1,k+1)
+
+print_items(4)
+
+           
+              
+                  
+                  
+
+
+
+
+
+
