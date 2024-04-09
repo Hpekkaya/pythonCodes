4c70e1871562e49886895215dfba78a77eb7b200
Hpekkaya <Ahpek1970@yahoo.com>
2024-04-09 11:06:52 +0300

    n

--- 100DaysCode/16_OOP/main.py
+++ 100DaysCode/16_OOP/main.py
@@ -19,7 +19,7 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu","Siquirtle","Charmande"])
-table.add_column("Type", ["Electric","Water","Fire"])
+table.add_column("Type", ["æElectric","Water","Fire"])
 
 table.align = "l"
 
